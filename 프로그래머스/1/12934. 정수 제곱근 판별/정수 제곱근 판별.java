ec6f34cc77fb667cf6fa62035f2e0e211d2400fc
Ji Young <80839715+Zy0ung@users.noreply.github.com>
2024-11-21 17:47:24 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.10 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        long sqrt = (long) Math.sqrt(n);
+        
+        if(sqrt * sqrt == n){
+            return (sqrt+1) * (sqrt+1);
+        }
+        return -1;
+    }
+}
