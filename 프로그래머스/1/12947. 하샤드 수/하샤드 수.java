fc90356678fc31c80051044aa270fd1d9eed1737
Ji Young <80839715+Zy0ung@users.noreply.github.com>
2025-04-22 14:55:07 +0900

    [level 1] Title: 하샤드 수, Time: 0.06 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,14 @@
+class Solution {
+    public Boolean solution(int x) {
+        
+        String num = String.valueOf(x);
+        
+        int sum = 0;
+
+        for (int i = 0; i < num.length(); i++) {
+            sum += num.charAt(i) - '0';
+        }
+
+        return x % sum == 0;
+    }
+}
